adff24afdbd024f3788c9a1cd7a3e4d44b68eefb
lkeller-synaptics <137900191+lkeller-synaptics@users.noreply.github.com>
2025-11-17 12:06:48 +0100

    Add tflite vs llvmcpu tests for Keras models (#424)

--- tests/test_mbv2.py
+++ tests/test_mbv2.py
@@ -2,6 +2,7 @@ import pytest
 
 from torq.testing.comparison import compare_test_results
 from torq.testing.tensorflow import generate_layers_from_model
+from torq.testing.iree import llvmcpu_reference_results
 
 import tensorflow as tf
 
@@ -16,7 +17,7 @@ def get_full_model():
 
 layers = generate_layers_from_model(get_full_model())
 
-@pytest.fixture(params=layers.keys())
+@pytest.fixture(params=["full_model", *layers.keys()])
 def case_config(request):
  
   if "bn" in request.param.lower():
@@ -27,14 +28,27 @@ def case_config(request):
                         "block_6_depthwise", "block_13_depthwise"]:
     pytest.xfail()
 
+  if request.param == "full_model":
+      keras_model = get_full_model().get_config()
+  else:
+      keras_model = layers[request.param]
+
   return {
             "keras_model": "layer_model",
-            "keras_layer_data": layers[request.param],
+            "keras_layer_data": keras_model,
             "mlir_model_file": "tflite_mlir_model_file",
             "tflite_model_file": "quantized_tflite_model_file",
             "input_data": "tweaked_random_input_data"
         }
 
 
-def test_model(request, tflite_reference_results, torq_results, case_config):
+def test_model_tflite_torq(request, tflite_reference_results, torq_results, case_config):
     compare_test_results(request, torq_results, tflite_reference_results, case_config)
+
+
+def test_model_llvmcpu_torq(request, llvm_reference_results, torq_results, case_config):
+    compare_test_results(request, torq_results, llvm_reference_results, case_config)
+
+
+def test_model_llvmcpu_tflite(request, tflite_reference_results, llvm_reference_results, case_config):
+    compare_test_results(request, tflite_reference_results, llvm_reference_results, case_config)
