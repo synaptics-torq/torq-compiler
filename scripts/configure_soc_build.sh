f5f8c3d9fa2599b5328cb23ae6ebcf205419f2bc
Dharini Subramaniam <150340453+dharini1703@users.noreply.github.com>
2026-01-26 11:38:11 +0530

    [Runtime] Enable runtime python bindings on SoC

--- scripts/configure_soc_build.sh
+++ scripts/configure_soc_build.sh
@@ -32,6 +32,8 @@ if [[ "$TARGET" == "astra_machina" ]]; then
         echo "Sourcing Astra SDK environment..."
         . "${SDK_DIR}/environment-setup-cortexa55-poky-linux"
         TOOLCHAIN_FILE="${SDK_DIR}/sysroots/x86_64-pokysdk-linux/usr/share/cmake/cortexa55-poky-linux-toolchain.cmake"
+        PYTHON3="${SDK_DIR}/sysroots/x86_64-pokysdk-linux/usr/bin/python3"
+        PYVER="$("${PYTHON3}" -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')"
     else
         # Default to aarch64 toolchain
         TOOLCHAIN_FILE=${BASE_DIR}/scripts/toolchain.aarch64.cmake
@@ -44,15 +46,34 @@ fi
 
 cd ${BASE_DIR}
 
-echo "Configuring build for target: $TARGET"
+CMAKE_ARGS=(
+  -B "${SOC_BUILD_DIR}"
+  -G Ninja
+  -DIREE_HOST_BIN_DIR="${HOST_BUILD_DIR}/third_party/iree/tools"
+  -DCMAKE_BUILD_TYPE=Release
+  -DIREE_BUILD_COMPILER=OFF
+  -DTORQ_ENABLE_AWS_FPGA=OFF
+  -DTORQ_ENABLE_SOC_FPGA="${TORQ_ENABLE_SOC_FPGA}"
+  -DTORQ_ENABLE_ASTRA_MACHINA="${TORQ_ENABLE_ASTRA_MACHINA}"
+  -DTORQ_ENABLE_SIMULATOR=OFF
+  -DIREE_HAL_DRIVER_LOCAL_SYNC=ON
+  -DIREE_HAL_DRIVER_LOCAL_TASK=ON
+  -DIREE_ENABLE_CPUINFO=OFF
+  -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE}"
+)
 
-cmake -B ${SOC_BUILD_DIR} \
-            -G Ninja \
-            -DIREE_HOST_BIN_DIR=${HOST_BUILD_DIR}/third_party/iree/tools \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DIREE_BUILD_COMPILER=OFF \
-            -DTORQ_ENABLE_AWS_FPGA=OFF \
-            -DTORQ_ENABLE_SOC_FPGA=${TORQ_ENABLE_SOC_FPGA} \
-            -DTORQ_ENABLE_ASTRA_MACHINA=${TORQ_ENABLE_ASTRA_MACHINA} \
-            -DTORQ_ENABLE_SIMULATOR=OFF \
-            -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+if [[ "$TARGET" == "astra_machina" && "$4" == "poky" ]]; then
+  CMAKE_ARGS+=(
+    -DIREE_BUILD_PYTHON_BINDINGS=ON
+    -DPython3_EXECUTABLE="${PYTHON3}"
+    -DPython3_INCLUDE_DIR="${SDK_DIR}/sysroots/cortexa55-poky-linux/usr/include/python${PYVER}"
+    -DPython_INCLUDE_DIR="${SDK_DIR}/sysroots/cortexa55-poky-linux/usr/include/python${PYVER}"
+    -DPython_NumPy_INCLUDE_DIR="${SDK_DIR}/sysroots/cortexa55-poky-linux/usr/lib/python${PYVER}/site-packages/numpy/core/include"
+    -DPython3_NumPy_INCLUDE_DIR="${SDK_DIR}/sysroots/cortexa55-poky-linux/usr/lib/python${PYVER}/site-packages/numpy/core/include"
+  )
+else
+  CMAKE_ARGS+=(-DIREE_BUILD_PYTHON_BINDINGS=OFF)
+fi
+
+echo "Configuring build for target: $TARGET"
+cmake "${CMAKE_ARGS[@]}"
