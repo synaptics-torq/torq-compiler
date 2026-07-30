d04136041fc1caed9ea749ee164c449978846125
chanha park-enerzai <156642471+chanhapark-enerzai@users.noreply.github.com>
2026-07-30 20:54:37 +0900

    [Compiler][EK] vectorize torq_hl.convert
    We are not vectorizing `torq_hl.convert`. It is basically the same pattern that `BroadcastPattern::transform` uses, 
    but without `expandDims` and `broadcastAs`.

--- compiler/torq/Conversions/TorqHLToTorqHW/ConvertPattern.cpp
+++ compiler/torq/Conversions/TorqHLToTorqHW/ConvertPattern.cpp
@@ -20,16 +20,31 @@ namespace mlir::syna::torq {
 template <>
 LogicalResult ConvertPattern::transform(torq_hl::ConvertOp op, PatternRewriter &rewriter) const {
     // The input and output can have any number of dimensions with any stride
+    struct In : Vectorized {
+        enum { NonDenseDims };
+    };
+
     Slice slice("convert");
     LData input(op.getInput());
     LData output(op.getInit());
     assert(input.dims() == output.dims() && "Input and output tensors must have the same shape");
 
-    For(auto ii = slice.iterate(input.dims())) {
-        IData idata = slice.iram.load(input[ii]);
-        PData pdata = slice.alu.load(idata);
-        QData res = slice.act.load(pdata);
-        slice.store(output[ii], res);
+    if (isCompressed(input.elementType())) {
+        return op.emitError() << "unsupported sub-byte element type "
+                              << cast<MemRefType>(op.getInput().getType()).getElementType();
+    }
+
+    // Vectorize input
+    int vectorSize = slice.act.width(input.elementType());
+    input.fuse(std::min(input.denseDims(), output.denseDims())).vectorize(vectorSize);
+
+    For(auto ndd = slice.iterate(input.dims(In::NonDenseDims, In::Vectors))) {
+        For(auto iv = slice.iterate(input.dim(In::Vectors))) {
+            IData idata = slice.iram.load(input[ndd][iv]);
+            PData pdata = slice.alu.load(idata);
+            QData res = slice.act.load(pdata);
+            slice.append(output[ndd], res);
+        }
     }
 
     rewriter.replaceOpWithNewOp<SliceTaskOp>(
